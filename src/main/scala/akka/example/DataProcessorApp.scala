b5c9374722401b9b3027bd360461c5b729f21e77
Dustin Vannoy <dustin-vannoy@pluralsight.com>
2017-07-17 15:50:55 -0700

    Group stuff

--- src/main/scala/akka/example/DataProcessorApp.scala
+++ src/main/scala/akka/example/DataProcessorApp.scala
@@ -5,73 +5,30 @@ import java.util.concurrent.TimeUnit
 import akka.actor.{ActorRef, ActorSystem}
 import akka.event.Logging
 import akka.example.Job.{CopyFile, PrintFile}
-import akka.example.JobManager.{CreateJob, KillJob}
 import akka.util.Timeout
-import util.JobInstruction.{CopyFileInstruction, PrintFileInstruction}
 
-import scala.io.StdIn
-import util.{JobInstruction, Terminal}
 
-import scala.annotation.tailrec
+import scala.util.Random
 
 /**
   * Created by dustin-vannoy on 7/6/17.
   */
-class DataProcessorApp(system: ActorSystem) extends Terminal {
+class DataProcessorApp(system: ActorSystem) {
 
   private val log = Logging(system, getClass.getName)
   implicit private val dataProcessorTimeout = Timeout(100, TimeUnit.SECONDS)
 
-  private val manager = createManager()
-
-  def createManager(): ActorRef =
-  system.actorOf(JobManager.props(), "job-manager")
 
   def run(): Unit = {
-    commandLoop()
+    val j = createJob(Random.alphanumeric.take(12).mkString)
+    j ! PrintFile("/opt/data/sample_file.csv")
   }
 
-  @tailrec
-  private def commandLoop(): Unit = {
-    println("Enter instructions...")
-    println("  j <id> = create new job")
-    println("  jc <instruction> = send new instruction")
-    println("  kill <actor_path> = kill a job")
-    Command(StdIn.readLine()) match {
-      case Command.Job(id) =>
-        manager ! CreateJob(id)
-        commandLoop
-      case Command.JobCommand(job: String, instruction: JobInstruction, parameter1: String, parameter2: String) =>
-        instruction match {
-          case PrintFileInstruction =>
-            val jobRef = system.actorSelection(job)
-            jobRef ! PrintFile(parameter1)
-          case CopyFileInstruction =>
-            val jobRef = system.actorSelection(job)
-            jobRef ! CopyFile(parameter1, parameter2)
-        }
-        commandLoop
-      case Command.Quit =>
-        system.terminate
-      case Command.Kill(job: String) =>
-        log.info("Requesting to kill {}", job)
-        manager ! KillJob(job)
-//        val jobRef = system.actorSelection("user/job-manager/" + job)
-//        jobRef ! PoisonPill
-//        jobRef.resolveOne().onComplete() {
-//          case Success(actorRef) => actorRef ! PoisonPill
-//          case Failure(ex) => log.error("Cannot kill job, user/job-manager/" + job + " does not exist")
-//        }
-      case Command.Unknown(command) =>
-        log.warning("Unknown command {}", command)
-        commandLoop
-    }
-  }
 
-//  import system.dispatcher
-//  protected def status(): Unit = {
-//    val response = processor ? DataProcessor.GetStatus
-//  }
+  private def createJob(id: String): ActorRef = {
+    val job: ActorRef = system.actorOf(Job.props(id), name = id)
+    job
+  }
 
 }
 
